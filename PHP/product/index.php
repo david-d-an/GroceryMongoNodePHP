c229cddf5ed3357faaf0652b529104969c394007
David An <dong82@gmail.com>
2018-12-21 09:57:32 -0500

    Reorganized FormValidation package

--- PHP/product/index.php
+++ PHP/product/index.php
@@ -2,10 +2,16 @@
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/contents.css">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" href="../packages/FormValidation/css/formValidation.css"/>
+  <script type="text/javascript" src="../packages/FormValidation/js/formValidation.js"></script>
+  <script type="text/javascript" src="../packages/FormValidation/js/framework/bootstrap.js"></script>
 </head>
 
 <?php
